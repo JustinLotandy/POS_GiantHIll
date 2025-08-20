cce859ada9ddeec8aaca19dcc8623f55c0d76c28
Justin Lotandy <03081220033@student.uph.edu>
2025-08-20 11:00:50 +0700

    a
    a

--- resources/views/partials/lowstock-toast.blade.php
+++ resources/views/partials/lowstock-toast.blade.php
@@ -4,12 +4,12 @@
     x-init="setTimeout(() => show = false, 5000)"
     x-show="show"
     x-transition:enter="transition ease-out duration-300"
-    x-transition:enter-start="opacity-0 translate-y-2"
+    x-transition:enter-start="opacity-0 -translate-y-2"
     x-transition:enter-end="opacity-100 translate-y-0"
     x-transition:leave="transition ease-in duration-200"
     x-transition:leave-start="opacity-100 translate-y-0"
-    x-transition:leave-end="opacity-0 translate-y-2"
-    class="fixed bottom-5 right-5 z-[1000] w-96 max-w-[95vw] bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-xl shadow-lg"
+    x-transition:leave-end="opacity-0 -translate-y-2"
+    class="fixed top-16 right-5 z-[1000] w-96 max-w-[95vw] bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-xl shadow-lg"
 >
     <div class="px-4 py-3 border-b border-gray-100 dark:border-gray-700 flex items-center justify-between">
         <div class="font-semibold text-gray-800 dark:text-gray-100">
